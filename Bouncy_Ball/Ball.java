0939460b1f53ca0326a258d1a0d2ac59027cb322
MiguelDelaSerna <111334239+MiguelDelaSerna@users.noreply.github.com>
2022-09-08 22:36:55 -0500

    Añadir Bouncy Ball Corregido

--- Bouncy_Ball/Ball.java
+++ Bouncy_Ball/Ball.java
@@ -2,11 +2,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Ball extends Actor
 {
+    private int offsetY = 1;
+    private int offsetX = 1;
+    
     public void act()
     {
-        move(5);
-        if(isAtEdge()){
-            turn(15);
-    }
+        int x = getX();
+        int y = getY();
+        
+        x += offsetX;
+        y += offsetY;
+        
+        if(y >= getWorld().getHeight() || y <=0){
+            offsetY *= -1;
+        }
+        
+        if(x >= getWorld().getWidth() || x <=0){
+            offsetX *= -1;
+        }
+        
+        setLocation(x, y);
     }
 }
